acab11f55e2c563db3b0976d51a71828fd0c9020
Diogo Correia <dv_correia@hotmail.com>
2024-10-04 01:35:26 +0100

    example on how to receive gatt writes from a central

--- firmware/nano33-ble/nano33-ble.ino
+++ firmware/nano33-ble/nano33-ble.ino
@@ -2,11 +2,13 @@
 // #include <Arduino_LSM9DS1.h>
 
 // Bluetooth® Low Energy Custom Cycle Time Service
-BLEService cycleTimeService("FFF0");
+BLEService configService("FFF0");
 
 // Cycle Time Characteristic
-// remote clients will be able to get notifications if this characteristic changes
-BLEIntCharacteristic ts("FFF1", BLERead | BLENotify);
+// remote clien will be able to get notifications if this characteristic changes
+BLEIntCharacteristic profileCharacteristic("FFF1", BLERead | BLEWrite);
+
+long previousMillis = 0;  // last time the cycle time was checked, in ms
 
 void exit(int code) {
   if (code != 0) {
@@ -23,15 +25,27 @@ void setup() {
   // initialize the built-in LED pin to indicate when a central is connected
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+
   if (!BLE.begin()) {
     Serial.println("error: failed to initialize BLE!");
     exit(1);
   }
   
+  /* Set a local name for the Bluetooth® Low Energy device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this Bluetooth® Low Energy device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setDeviceName("NERVOUS-SYS-BLE");
   BLE.setLocalName("NERVOUS-SYS-BLE");
-  BLE.setAdvertisedService(cycleTimeService); // add service UUID
-  cycleTimeService.addCharacteristic(ts); // add the cycle time characteristic
-  BLE.addService(cycleTimeService); // Add the cycle time service
+  BLE.setAdvertisedService(configService); // add service UUID
+  configService.addCharacteristic(profileCharacteristic); // add the cycle time characteristic
+  BLE.addService(configService); // Add the cycle time service
 
   /* Start advertising Bluetooth® Low Energy. It will start continuously transmitting Bluetooth® Low Energy
      advertising packets and will be visible to remote Bluetooth® Low Energy central devices
@@ -63,7 +77,16 @@ void loop() {
       // if 200ms have passed, update the cycle time:
       if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
-        Serial.println("wewe")
+        Serial.println("connected to central");
+      }
+
+      if (profileCharacteristic.written()) {
+        // Read the value that was written by the central
+        byte value = profileCharacteristic.value();
+
+        // Do something with the value
+        Serial.print("Value written by central: ");
+        Serial.println(value);
       }
     }
 
